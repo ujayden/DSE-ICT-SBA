be65f63fb70d0ece34be7963ef3549fad6d37ef0
Uncle Jayden <82523546+uncle-jayden@users.noreply.github.com>
2023-08-18 01:53:11 +0800

    feat: Add styling and script to drop-down menu
    The content of the drop-down menu will be add/remove based on item does/ does not display on the main header.
    The menu of the link is hidden default now and can be toggle on/of.
    Change the background color of breadcrumbs to make it fit with the drop-down menu of header.

--- include/script.js
+++ include/script.js
@@ -12,15 +12,17 @@ function toggleDropDownMenu(menuElm) {
     }
 }
 //Use JavaScript selector to select the element of menu content.
-let menu_ResLibrary = document.querySelector('#dropdownMenu_ResLibrary .dropdownContent');
-let meun_Courses = document.querySelector('#dropdownMenu_Courses .dropdownContent');
+let menu_ResLibrary_btn = document.getElementById('dropdownMenu_ResLibrary');
+let menu_Courses_btn = document.getElementById('dropdownMenu_Courses');
+let menu_ResLibrary = document.getElementById('dropdownMenu_ResLibrary-List');
+let meun_Courses = document.getElementById('dropdownMenu_Courses-List');
 
 //Add event listener to the header-nav button.
-document.getElementById('dropdownMenu_ResLibrary').addEventListener('click', function() {
+menu_ResLibrary_btn.addEventListener('click', function() {
     toggleDropDownMenu(menu_ResLibrary);
 }
 );
-document.getElementById('dropdownMenu_Courses').addEventListener('click', function() {
+menu_Courses_btn.addEventListener('click', function() {
     toggleDropDownMenu(meun_Courses);
 }
 );
@@ -39,5 +41,37 @@ let menu_Header = document.getElementById('headerDropDownMenu');
 //Add event listener to the #headerDropDownMenu-Logo
 document.getElementById('headerDropDownMenu-Logo').addEventListener('click', function() {
     toggleDropDownMenu(menu_Header);
+}
+);
+document.getElementById('headerDropDownMenu-Close').addEventListener('click', function() {
+    toggleDropDownMenu(menu_Header);
+}
+);
+//Only function for menu.
+function toggleDropDownMenuFlex_headerDDMenu(menuElm, callElm) {
+    console.log(menuElm);
+    //The menuElm should be the element it self, not the id of the element.
+    if (!(menuElm.style.display == 'flex')) {
+        menuElm.style.display = 'flex';
+        //Change the Status from "Close" to "Open"
+        callElm.querySelector('.headerDropDownMenu-Link-Status').innerHTML = '&#9660;';
+    }else{
+        //Use removeAttribute to remove the style attribute => Make sure css of hover menu can be run.
+        menuElm.removeAttribute('style');
+        //Change the Status from "Open" to "Close"
+        callElm.querySelector('.headerDropDownMenu-Link-Status').innerHTML = '&#9654;';
+    }
+}
+
+let headerDDMenu_Course_btn = document.getElementById('headerDropDownMenu-Link-Courses');
+let headerDDMenu_Course_list = document.getElementById('headerDropDownMenu-Link-Courses-List');
+let headerDDMenu_ResLibrary_btn = document.getElementById('headerDropDownMenu-Link-ResLibrary');
+let headerDDMenu_ResLibrary_list = document.getElementById('headerDropDownMenu-Link-ResLibrary-List');
+headerDDMenu_Course_btn.addEventListener('click', function() {
+    toggleDropDownMenuFlex_headerDDMenu(headerDDMenu_Course_list, headerDDMenu_Course_btn);
+}
+);
+headerDDMenu_ResLibrary_btn.addEventListener('click', function() {
+    toggleDropDownMenuFlex_headerDDMenu(headerDDMenu_ResLibrary_list, headerDDMenu_ResLibrary_btn);
 }
 );

f4c79430552281c1ec61da12c22703c55a6482e6
Uncle Jayden <82523546+uncle-jayden@users.noreply.github.com>
2023-11-23 00:35:14 +0800

    Style: Change the style of side bar

--- include/global/slidebar.js
+++ include/global/slidebar.js
@@ -1,26 +1,32 @@
 'use strict';
-function showNavbar(toggleId, navId, bodyId, headerId){
-    const toggle = document.getElementById(toggleId),
-      nav = document.getElementById(navId),
-      bodypd = document.getElementById(bodyId),
-      headerpd = document.getElementById(headerId);
-  
-    // Validate that all variables exist
-    if (toggle && nav && bodypd && headerpd) {
-      toggle.addEventListener('click', () => {
-        // show navbar
-        nav.classList.toggle('show');
-        // change icon
-        toggle.classList.toggle('bx-x');
-        // add padding to body
-        bodypd.classList.toggle('body-pd');
-        // add padding to header
-        headerpd.classList.toggle('body-pd');
-      });
-    }
-  };
-  
-showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
+let toggle = document.getElementById('header-toggle'),
+  nav = document.getElementById('nav-bar'),
+  bodypd = document.getElementById('body-pd'),
+  headerpd = document.getElementById('header');
+
+let grayBackground = document.getElementById('greyBackground');
+
+function toggleNavBar() {
+    nav.classList.toggle('show');
+    toggle.classList.toggle('bx-x');
+    bodypd.classList.toggle('body-pd');
+    headerpd.classList.toggle('body-pd');
+	if (grayBackground.style.visibility != 'visible') {
+		grayBackground.style.visibility = 'visible';
+	} else {
+		grayBackground.style.visibility = 'hidden';
+	}
+}
+toggle.addEventListener('click', () => {
+	toggleNavBar();
+});
+grayBackground.addEventListener('click', () => {
+	toggleNavBar();
+});
+let navSelfClose = document.getElementById('navCloseSelf');
+navSelfClose.addEventListener('click', () => {
+	toggleNavBar();
+});
 let linkColor = document.querySelectorAll('.nav_link');
   
 function colorLink() {

41da806e1c41856fc71d375f0cc088c7d6893f93
Uncle Jayden <82523546+uncle-jayden@users.noreply.github.com>
2023-12-10 03:12:08 +0800

    Fix: Fix Hint cannot be close in quiz1.html

--- include/discussionForum/script.js
+++ include/discussionForum/script.js
@@ -7,6 +7,23 @@ selectChannelDropDown.addEventListener("change", function () {
     console.log(currentChannel);
 });
 
+let alertPopup = document.getElementById("postAlert");
+let alertContainer = document.getElementById("postAlertContent");
+
+/**
+ * 
+ * @param {string} message // The message to be displayed in the alert
+ * @param {string} alertHeader // The alert header type
+ */
+function updateAlert(message, alertHeader) {
+    alertPopup.classList.add("show");
+    alertContainer.innerHTML = `
+        <strong>${alertHeader}</strong>${message}
+    `;
+    setTimeout(function () {
+        alertPopup.classList.remove("show");
+    }, 5000);
+}
 
 let currentURL = new URL(window.location.href);
 let searchBox = document.getElementById("search");
@@ -250,5 +267,252 @@ function displayPostDetail(post) {
 
         // Append contentDiv to postContentContainer
         postContentContainer.appendChild(contentDiv);
+    });
+}
+
+if (typeof marked == 'undefined' && typeof DOMPurify == 'undefined') {
+    throw new Error('Mark.js and DOMPurify not available, check if the script is loaded');
+}
+/**
+ * Converts raw content to markdown using marked library and sanitizes it.
+ * @param {string} toMarkdown - The text to be converted to markdown
+ * @returns {string} - Sanitized markdown text
+ */
+function generateMarkdownText(toMarkdown) {
+    let markdownText = marked.parse(toMarkdown);
+    markdownText = DOMPurify.sanitize(markdownText);
+    return markdownText;
+}
+
+let titleInput = document.getElementById('newPostTitle');
+let contentTextarea = document.getElementById('newPostContent');
+let tagsInput = document.getElementById('newPostTags');
+let postContainer = document.getElementById('previewContent');
+let previewModal = document.getElementById('previewModal');
+
+
+/**
+ * Generates a post preview and optionally displays it in a Bootstrap modal.
+ * @param {string} title - Title obtained from the title input
+ * @param {string} rawContent - Raw content obtained from the textarea
+ * @param {string} tag - Tag obtained from the tag input
+ * @param {string} currentChannel - Current channel obtained from the channel dropdown
+ * @param {HTMLElement} targetContainer - The container to append the post to
+ * @param {HTMLElement} targetPopUpElem - The Bootstrap modal to show the preview, if false, no preview will be shown
+ */
+function generatePreview(title, rawContent, tag, currentChannel, targetContainer, targetPopUpElem) {
+    // Create the preview container
+    let previewContainer = document.createElement('div');
+    previewContainer.classList.add('preview-post');
+
+    // Generate markdown text
+    let markdownText = generateMarkdownText(rawContent);
+    
+    //Make title and tag to html characters to prevent XSS - he.js
+    title = he.encode(title);
+    tag = he.encode(tag);
+
+
+    // Fill the preview container with post details
+    previewContainer.innerHTML = `
+        <h2>${title}</h2>
+        <p><strong>Channel:</strong> ${currentChannel}</p>
+        <div>${markdownText}</div>
+        <p><strong>${tag}</strong></p>
+    `;
+
+    // Append the preview to the target container
+    targetContainer.innerHTML = '';
+    targetContainer.appendChild(previewContainer);
+
+    // Display the preview in the Bootstrap modal if provided
+    if (targetPopUpElem) {
+        // Update the content of the modal body with the generated preview
+        let modalBody = targetPopUpElem.querySelector('.modal-body');
+        modalBody.innerHTML = '';
+        modalBody.appendChild(previewContainer);
+
+        // Show the modal
+        new bootstrap.Modal(targetPopUpElem).show();
+    }
+}
+
+previewBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    generatePreview(
+        titleInput.value,
+        contentTextarea.value,
+        tagsInput.value,
+        selectChannelDropDown.value,
+        postContainer,
+        previewModal
+    );
+});
+
+function convertToHashtags() {
+    if (tagsInput) {
+        let textContent = tagsInput.value;
+        if (textContent.match(/#\s+/)) {
+            tagsInput.value = textContent.replace(/#\s+/, '#');
+            return false;
+        }
+        // Also prevent ## from being entered
+        if (textContent.match(/##/)) {
+            tagsInput.value = textContent.replace(/##/, '#');
+            return false;
+        }
+        let words = textContent.split(/\s+/); // Use a regex to split by any whitespace
+        let hashtags = words.map(word => word.startsWith('#') ? word : '#' + word);
+        tagsInput.value = hashtags.join(' ');
+    }
+}
+
+tagsInput.addEventListener('input', convertToHashtags);
+
+let addAttachmentBtn = document.getElementById('addAttachmentBtn');
+
+addAttachmentBtn.addEventListener('click', function() {
+    handleAttachment();
+});
+
+let attachmentList = document.getElementById('attachmentContainer');
+
+let newPostFiles = [];
+
+function handleAttachment() {
+    // Clear the list
+    attachmentList.innerHTML = '';
+    newPostFiles = [];
+
+    // Open the file dialog
+    let fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.multiple = true;
+    fileInput.accept = '.zip, .rar, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.ms-powerpoint, application/vnd.openxmlformats-officedocument.presentationml.presentation, image/*, video/*, .svg, text/html, text/css, application/javascript';
+    fileInput.click();
+
+    // List the files selected
+    fileInput.addEventListener('change', function (event) {
+        let files = event.target.files;
+        let totalSize = 0;
+
+        // Maximum file size is 8MB, and up to 3 files are allowed
+        for (let i = 0; i < files.length; i++) {
+            let file = files[i];
+
+            // Check file size
+            if (file.size > 8 * 1024 * 1024) {
+                alert('File size exceeds the maximum limit of 8MB.');
+                continue;
+            }
+
+            // Check the number of files
+            if (i >= 3) {
+                alert('You can select up to 3 files.');
+                break;
+            }
+
+            let fileItem = document.createElement('div');
+            fileItem.classList.add('file-item');
+
+            let fileName = document.createElement('span');
+            fileName.classList.add('file-name');
+            fileName.textContent = file.name;
+
+            let fileSize = document.createElement('span');
+            fileSize.classList.add('file-size');
+            fileSize.textContent = ` ${(file.size / (1024 * 1024)).toFixed(2)} MB`;
+
+            fileItem.appendChild(fileName);
+            fileItem.appendChild(fileSize);
+
+            attachmentList.appendChild(fileItem);
+
+            totalSize += file.size;
+
+            // Push the selected file to the array
+            newPostFiles.push(file);
+            console.log('Processing Files:', newPostFiles);
+
+        }
+
+        console.log('Selected Files:', newPostFiles);
+        console.log('Total Size:', totalSize, 'bytes');
+    });
+}
+
+let resetBtn = document.getElementById('cleanBtn');
+let newPostForm = document.getElementById('newPostForm');
+resetBtn.addEventListener('click', function() {
+    // Reset the form
+    newPostForm.reset();
+});
+
+
+//Handle the form submission
+newPostForm.addEventListener('submit', function(event) {
+    handleSubmission(event);
+}
+);
+let originURL = new URL(window.location.href).origin;
+function handleSubmission(event) {
+    event.preventDefault();
+
+    // Get form data as JSON
+    let filesTrackerUUID = crypto.randomUUID();
+    let formData = {
+        mode: 'newPost',
+        title: $('#newPostTitle').val(),
+        category: $('#currentCategory').text(),
+        content: $('#newPostContent').val(),
+        tags: $('#newPostTags').val(),
+        filesTrackerUUID: filesTrackerUUID
+    };
+
+    // Make an AJAX request
+    $.ajax({
+        url: originURL + '/api/discuss.php',
+        type: 'POST',
+        contentType: 'application/json', // Specify content type as JSON
+        data: JSON.stringify(formData), // Convert data to JSON string
+        success: function(response) {
+            // Handle success response
+            console.log('Post submitted successfully:', response);
+        },
+        error: function(error) {
+            // Handle error response
+            console.error('Error submitting post:', error);
+            updateAlert('Sorry, there was an error submitting your post. Please try again later.', 'An error occurred: ');
+        }
+    });
+    // Upload the files
+    uploadFiles(filesTrackerUUID);
+}
+function uploadFiles(filesTrackerUUID) {
+    // Create FormData object for file uploads
+    var fileFormData = new FormData();
+
+    // Append files and associated tracker UUID to FormData
+    for (var i = 0; i < newPostFiles.length; i++) {
+        fileFormData.append('files[]', newPostFiles[i]);
+    }
+    fileFormData.append('filesTrackerUUID', filesTrackerUUID);
+
+    // Make the second AJAX request to upload files
+    $.ajax({
+        url: originURL + '/api/uploadFiles.php', // Update with the actual upload endpoint
+        type: 'POST',
+        data: fileFormData,
+        processData: false,
+        contentType: false,
+        success: function(response) {
+            // Handle success response
+            console.log('Files uploaded successfully:', response);
+        },
+        error: function(error) {
+            // Handle error response
+            console.error('Error uploading files:', error);
+            updateAlert('Sorry, there was an error uploading your files. Please try again later.', 'An error occurred: ');
+        }
     });
 }
